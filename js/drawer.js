5444f855dc5c62c0efdb75ce7e84346fbe176d04
Armando Villamil <119384839+Arvillax@users.noreply.github.com>
2026-04-14 03:12:45 -0600

    falta corregir rutas

--- js/drawer.js
+++ js/drawer.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Drawer Menu (Abrir/Cerrar)
+
     const menuToggle = document.getElementById('menuToggle');
     const menuClose = document.getElementById('menuClose');
     const navLinksContainer = document.getElementById('navLinksContainer');
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    // Submenús en Móvil (Acordeón)
+
     const dropdowns = document.querySelectorAll('.menu-item-has-children');
     dropdowns.forEach(dropdown => {
         const link = dropdown.querySelector('a');
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // Cerrar el drawer al hacer clic fuera de él 
+
     document.addEventListener('click', (e) => {
         if (navLinksContainer && navLinksContainer.classList.contains('active')) {
             if (!navLinksContainer.contains(e.target) && !menuToggle.contains(e.target)) {
